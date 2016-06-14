628f4bc2d2aa7fda3c83fec7a93a9d54bd5bc015
Nagarro <shubham.baghwala@nagarro.com>
2016-06-14 10:20:57 +0530

    uncomment code

--- Demo.java
+++ Demo.java
@@ -53,7 +53,7 @@ public class Demo {
 	        return false;
 	    }
 	    
-	public static void main(String[] args) //throws Exception
+	public static void main(String[] args) throws Exception
 	{
 		System.out.println("Inside start of Main Method");
 		System.out.println(isRestrictedBranch("master,release"));
